14d8c9aca12be5bda0857c2d2f08d78aefb096de
MRAGANK GAUTAM <145200189+Mragankk@users.noreply.github.com>
2024-05-05 18:03:52 +0530

    Update ProductTracking.js

--- test/ProductTracking.js
+++ test/ProductTracking.js
@@ -1,43 +1,42 @@
-const { expect } = require("chai");
+const { expect } = require('chai');
 
-describe("ProductTracking", function () {
+describe('ProductTracking', function () {
   let ProductTracking;
   let productTracking;
   let owner;
   let addr1;
   let addr2;
-  let addrs;
 
-  before(async function () {
-    ProductTracking = await ethers.getContractFactory("ProductTracking");
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+  beforeEach(async function () {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    ProductTracking = await ethers.getContractFactory('ProductTracking');
     productTracking = await ProductTracking.deploy();
   });
 
-  describe("registerProduct", function () {
-    it("should register a product", async function () {
-      await productTracking.registerProduct(1, "Product 1", "Details 1");
-
-      const productDetails = await productTracking.products(1);
-      expect(productDetails.name).to.equal("Product 1");
-      expect(productDetails.supplyChainDetails).to.equal("Details 1");
-      expect(productDetails.manufacturer).to.equal(owner.address);
-    });
+  it('Should register a product', async function () {
+    await productTracking.registerProduct(1, 'Product 1', 'Details 1');
+    const product = await productTracking.products(1);
+    expect(product.name).to.equal('Product 1');
+    expect(product.supplyChainDetails).to.equal('Details 1');
+    expect(product.manufacturer).to.equal(owner.address);
   });
 
-  describe("getProductDetails", function () {
-    it("should return correct product details", async function () {
-      await productTracking.registerProduct(2, "Product 2", "Details 2");
+  it('Should not register a product if already registered', async function () {
+    await productTracking.registerProduct(1, 'Product 1', 'Details 1');
+    await expect(productTracking.registerProduct(1, 'Product 2', 'Details 2'))
+      .to.be.revertedWith('Product already registered');
+  });
 
-      const productDetails = await productTracking.getProductDetails(2);
-      expect(productDetails.name).to.equal("Product 2");
-      expect(productDetails.supplyChainDetails).to.equal("Details 2");
-      expect(productDetails.manufacturer).to.equal(owner.address);
-    });
+  it('Should get product details', async function () {
+    await productTracking.registerProduct(1, 'Product 1', 'Details 1');
+    const productDetails = await productTracking.getProductDetails(1);
+    expect(productDetails.name).to.equal('Product 1');
+    expect(productDetails.supplyChainDetails).to.equal('Details 1');
+    expect(productDetails.manufacturer).to.equal(owner.address);
+  });
 
-    it("should revert for non-existing product", async function () {
-      // Try to get details of a non-existing product (ID 999)
-      await expect(productTracking.getProductDetails(999)).to.be.revertedWith("Product does not exist");
-    });
+  it('Should revert when getting details of non-existing product', async function () {
+    await expect(productTracking.getProductDetails(1))
+      .to.be.revertedWith('Product does not exist');
   });
 });
